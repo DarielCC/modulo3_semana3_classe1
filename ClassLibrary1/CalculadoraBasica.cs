c068f756d8167658ed0e3f9129ccdd8ad97c643e
Dariel Yamil Corrales Cuenca <dariel.cuenca@way2.com.br>
2022-05-24 19:52:34 -0300

    Criando novos testes

---
+++ ClassLibrary1/CalculadoraBasica.cs
@@ -0,0 +1,44 @@
+﻿namespace Calculadora.Library
+{
+    public class CalculadoraBasica
+    {
+        public int SomarDoisNumeros(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentException("Parámetro inválido");
+
+            if (b > 1000)
+                throw new ArgumentException("O segundo argumento não pode ser maior do que 1000");
+
+            return a + b;
+        }
+
+        public decimal MultiplicarNumeros(int a, int b, int c)
+        {
+            if (a < 0 || b < 0 || c < 0)
+            {
+                throw new ArgumentException("Parámetros inválidos");
+            }
+
+            return a * b * c;
+        }
+
+        public decimal DividirNumeros(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Denominador não pode ser zero");
+            }
+
+            if (a < 0 || b < 0)
+            {
+                throw new ArgumentException("Parámetro inválido");
+            }
+
+            return a / b;
+        }
+
+        public decimal DividirSoma(int a, int b, int c) 
+            => DividirNumeros(SomarDoisNumeros(a, b), c);
+    }
+}
---
+++ TestProject1/CalculadoraBasicaTest.cs
@@ -0,0 +1,226 @@
+using System;
+using Xunit;
+using Calculadora.Library;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraBasicaTest
+    {
+        [Fact]
+        [Trait("Soma", "Soma dois números")]
+        public void SomarDoisNumeros_SomaDoisNumero_ResultadoEsperado3_Antigo()
+        {
+            //AAA
+
+            //Arrange
+            var a = 1;
+            var b = 2;
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var soma = class1.SomarDoisNumeros(a, b);
+
+            //Assert
+            Assert.Equal(a + b, soma);
+        }
+
+        [Theory]
+        [Trait("Soma", "Soma dois números")]
+        [InlineData(1, 2, 3)]
+        [InlineData(10, 25, 35)]
+        [InlineData(100, 30, 130)]
+        [InlineData(6, 8, 14)]
+        public void SomarDoisNumeros_SomaDoisNumero_ResultadoEsperadoSoma(int a, int b, int result)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var soma = class1.SomarDoisNumeros(a, b);
+
+            //Assert
+            Assert.Equal(result, soma);
+        }
+
+
+        [Fact(Skip = "Fluxo não valido")]
+        public void SomarDoisNumeros_Erro_ResultadoEsperadoMenos1()
+        {
+            //AAA
+
+            //Arrange
+            var a = -50;
+            var b = 2;
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var soma = class1.SomarDoisNumeros(a, b);
+
+            //Assert
+            Assert.Equal(-1, soma);
+        }
+
+        [Theory]
+        [Trait("Soma", "Valida números negativos")]
+        [InlineData(-100, -30, "Parámetro inválido")]
+        [InlineData(-6, -8, "Parámetro inválido")]
+        public void SomarDoisNumeros_ArgumentosNegativos_RetornaArgumentException(int a, int b, string mensagem)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.SomarDoisNumeros(a, b));
+
+            //Assert
+            Assert.Equal(mensagem, ex.Message);
+        }
+
+        [Fact]
+        [Trait("Soma", "Valida argumento maior do que 1000")]
+        public void SomarDoisNumeros_ArgumentoInvalidoQuandoArgumentoMaiorQue1000_RetornaArgumentException_Fact()
+        {
+            //AAA
+
+            //Arrange
+            var a = 1;
+            var b = 1001;
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.SomarDoisNumeros(a, b));
+
+            //Assert
+            Assert.Equal("O segundo argumento não pode ser maior do que 1000", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(100, 2030, "O segundo argumento não pode ser maior do que 1000")]
+        [InlineData(6, 1001, "O segundo argumento não pode ser maior do que 1000")]
+        public void SomarDoisNumeros_ArgumentoInvalidoQuandoArgumentoMaiorQue1000_RetornaArgumentException_Theory(int a, int b, string mensagem)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.SomarDoisNumeros(a, b));
+
+            //Assert
+            Assert.Equal(mensagem, ex.Message);
+        }
+
+        [Theory]
+        [Trait("Multiplicação", "Multiplica 3 números")]
+        [InlineData(2, 3, 6, 36)]
+        [InlineData(1, 2, 3, 6)]
+        public void MultiplicarNumeros_MultiplicacaoDe3Numeros_RetornarValorDaMultiplicacao(int a, int b, int c, int valor)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var multiplicacao = class1.MultiplicarNumeros(a, b, c);
+
+            //Assert
+            Assert.Equal(valor, multiplicacao);
+        }
+
+        [Theory]
+        [Trait("Multiplicação", "Valida parámetros negativos")]
+        [InlineData(-1, 2, 4, "Parámetros inválidos")]
+        [InlineData(1, -2, 4, "Parámetros inválidos")]
+        [InlineData(1, 2, -4, "Parámetros inválidos")]
+        [InlineData(-1, -2, -4, "Parámetros inválidos")]
+        public void MultiplicarNumeros_ArgumentoInvalidoQuandoAlgumParametroNegativo_RetornaArgumentException(int a, int b, int c, string mensagem)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.MultiplicarNumeros(a, b, c));
+
+            //Assert
+            Assert.Equal(mensagem, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(3, 1, 3)]
+        [InlineData(6, 2, 3)]
+        [InlineData(15, 3, 5)]
+        public void DividirNumeros_DivisaoDe2Numeros_RetornarValorDaDivisao(int a, int b, int valor)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var divisao = class1.DividirNumeros(a, b);
+
+            //Assert
+            Assert.Equal(valor, divisao);
+        }
+
+        [Theory]
+        [InlineData(1, 0, "Denominador não pode ser zero")]
+        public void DividirNumeros_ArgumentoInvalidoQuandoSegundoParametroZero_RetornaArgumentException(int a, int b, string mensagem)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.DividirNumeros(a, b));
+
+            //Assert
+            Assert.Equal(mensagem, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1, 3, "Parámetro inválido")]
+        [InlineData(1, -1, "Parámetro inválido")]
+        [InlineData(-1, -3, "Parámetro inválido")]
+        public void DividirNumeros_ArgumentoInvalidoQuandoAlgumParametroNegativo_RetornaArgumentException(int a, int b, string mensagem)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => class1.DividirNumeros(a, b));
+
+            //Assert
+            Assert.Equal(mensagem, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(2, 1, 3, 1)]
+        [InlineData(6, 2, 2, 4)]
+        [InlineData(5, 5, 5, 2)]
+        public void DividirSoma_DivisaoDaSomaDe2NumerosComOutroNumero_RetornarValorDaDivisao(int a, int b, int c, int valor)
+        {
+            //AAA
+
+            //Arrange
+            var class1 = new CalculadoraBasica();
+
+            //Act
+            var divisao = class1.DividirSoma(a, b, c);
+
+            //Assert
+            Assert.Equal(valor, divisao);
+        }
+    }
+}

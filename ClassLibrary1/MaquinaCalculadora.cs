8cd00c37bfb23d119e981242e572c8330ba199a3
Dariel Yamil Corrales Cuenca <dariel.cuenca@way2.com.br>
2022-05-25 20:01:30 -0300

    Testando classe MaquinaCalculadora

--- ClassLibrary1/MaquinaCalculadora.cs
+++ ClassLibrary1/MaquinaCalculadora.cs
@@ -6,7 +6,25 @@
 
         public MaquinaCalculadora(ICalculadoraBasica calculadoraBasica) => _calculadoraBasica = calculadoraBasica;
 
-        public int Somar(int a, int b) => _calculadoraBasica.SomarDoisNumeros(a, b);    
+        public int? Somar(int a, int b)
+        {
+            if(a > b)
+                return -1;
+
+            if (b > a)
+                return null;
+
+            var soma = _calculadoraBasica.SomarDoisNumeros(a, b);
+
+            if(soma <= 10)
+                return 0;
+
+            a += b;
+
+            soma = _calculadoraBasica.SomarDoisNumeros(a, b);
+
+            return soma;
+        }
 
         public decimal Multiplicar(int a, int b, int c) => _calculadoraBasica.MultiplicarNumeros(a, b, c);
 
--- TestProject1/MaquinaCalculadoraTest.cs
+++ TestProject1/MaquinaCalculadoraTest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Calculadora.Tests
 {
@@ -17,6 +18,77 @@ namespace Calculadora.Tests
         {
             _mockCalculadoraBasica = new Mock<ICalculadoraBasica>();
             _maquinaCalculadora = new MaquinaCalculadora(_mockCalculadoraBasica.Object);
+        }
+
+        [Fact]
+        public void Somar_SomarDoisNumeros_Soma()
+        {
+            //Arrage
+            var a = 10;
+            var b = 2;
+            _mockCalculadoraBasica.SetupSequence(m => m.SomarDoisNumeros(a, b))
+                .Returns(a + b)
+                .Returns((a += b) + b);
+
+            //Act
+            var soma = _maquinaCalculadora.Somar(a, b);
+
+            //Assert
+            Assert.Equal((a += b) + b, soma);
+            _mockCalculadoraBasica.Verify(m => m.SomarDoisNumeros(a, b), Times.Once());
+        }
+
+
+        [Fact]
+        public void Somar_SomarDoisNumeros_ZeroQuandoSomaMenorQueZero()
+        {
+            //Arrage
+            var a = 1;
+            var b = 2;
+            _mockCalculadoraBasica.Setup(m => m.SomarDoisNumeros(a, b)).Returns(a + b);
+
+            //Act
+            var soma = _maquinaCalculadora.Somar(a, b);
+
+            //Assert
+            Assert.Equal(0, soma);
+            _mockCalculadoraBasica.Verify(m => m.SomarDoisNumeros(a, b), Times.Once());
+        }
+
+        [Fact]
+        public void Somar_SomarDoisNumeros_MenosUmQuandoAMaiorQueB()
+        {
+            //Arrage
+            var a = 10;
+            var b = 2;
+            _mockCalculadoraBasica.Setup(m => m.SomarDoisNumeros(a, b)).Returns(a + b);
+
+            //Act
+            var soma = _maquinaCalculadora.Somar(a, b);
+
+            //Assert
+            Assert.Equal(-1, soma);
+            Assert.NotNull(soma);
+            _mockCalculadoraBasica.Verify(m => m.SomarDoisNumeros(a, b), Times.Never());
+        }
+
+        [Fact]
+        public void Multiplicar_MultiplicaTresNumeros_Multiplicacao()
+        {
+            //Arrage
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            _mockCalculadoraBasica.Setup(m => m.MultiplicarNumeros(a, b, c))
+                .Returns(a * b * c);
+
+            //Act
+            var multiplicacao = _maquinaCalculadora.Multiplicar(a, b, c);
+
+            //Assert
+            Assert.Equal(a * b * c, multiplicacao);
+            _mockCalculadoraBasica.Verify(m => m.MultiplicarNumeros(a, b, c), Times.Once());
         }
     }
 }
